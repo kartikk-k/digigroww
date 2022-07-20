a372faa4335a14193ab81be84dd92238c052079c
Kartik Khorwal <hellokartikk@gmail.com>
2022-07-20 23:17:10 +0530

    first-update

---
+++ script.js
@@ -0,0 +1,9 @@
+function alert() {
+    document.getElementById("pop").style.cssText =
+        'opacity: 1; z-Index: 4;'
+}
+
+function alertclose() {
+    document.getElementById("pop").style.cssText =
+        'opacity: 0; z-Index: -1;'
+}
